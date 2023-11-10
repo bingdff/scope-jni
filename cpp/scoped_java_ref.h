c93c671b3ad54566f686413580a37f4cc5f4569a
bingdff <bingdff@gmail.com>
2023-11-10 14:36:58 +0800

    Update scoped_java_ref.h

--- cpp/scoped_java_ref.h
+++ cpp/scoped_java_ref.h
@@ -1,7 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+// Copyright (c) 2012 The Chromium Authors. All rights reserved. 
 #ifndef BASE_ANDROID_SCOPED_JAVA_REF_H_
 #define BASE_ANDROID_SCOPED_JAVA_REF_H_
 
